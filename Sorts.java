236c7ae9b240edefdc1e464bcfadd2f3ca54ca15
Aries <aho11@stuy.edu>
2020-12-17 14:18:31 -0500

    fixed selectionSort

--- Sorts.java
+++ Sorts.java
@@ -13,9 +13,13 @@ public class Sorts{
 
   public static void selectionSort(int[] data){
     for(int i = 0; i < data.length; i++){
-      int small = 0;
-      for(int j = 0; j < data.length-1; j++){
-        if(data[j] < data[j+1]) small = data[j];
+      int small = data[i];
+      int temp = small;
+      for(int j = i; j < data.length-1; j++){
+        if(small > data[j+1]){
+          small = data[j+1];
+          data[j+1] = temp;
+        }
       }
       data[i] = small;
     }
