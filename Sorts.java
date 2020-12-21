c649ac56d171f7d6f84fd728da27df8b3cb56b73
Aries <aho11@stuy.edu>
2020-12-21 13:30:53 -0500

    edited BubbleSort

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,7 @@
 public class Sorts{
   public static void bubbleSort(int[] data){
-      for(int i = data.length-1; i >= 0; i--){
-        for(int j = 0; j < i; j++){
+      for(int i = 0; i < data.length-1; i++){
+        for(int j = 0; j < data.length-i-1; j++){
           if(data[j] > data[j+1]){
             int temp = data[j+1];
             data[j+1] = data[j];
